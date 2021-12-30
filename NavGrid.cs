4b8ca797d5107c9075178ffc86939143cbcd282f
andrew.pirkl <andrew.pirkl@gmail.com>
2021-12-30 15:35:14 -0600

    round dict entries for better searches

--- NavGrid.cs
+++ NavGrid.cs
@@ -10,7 +10,7 @@ namespace AStarSquares
         private const int STRAIGHT_COST = 10;
         private const int DIAGONAL_COST = 14;
         
-        public IDictionary<Vector3, INavNode> AllNodes { get; private set;} = new Dictionary<Vector3, INavNode>();
+        public IDictionary<Vector3Int, List<INavNode>> AllNodes { get; private set;} = new Dictionary<Vector3Int, List<INavNode>>();
 
         private void Start() {
             foreach (INavNode node in GetComponentsInChildren<INavNode>())
@@ -30,17 +30,21 @@ namespace AStarSquares
                     neighborNode.NavNodeLinks.Add(new NavNodeLink(node, distance, vertical));
                 }
             });
-            AllNodes.Add(node.Anchor, node);
+            Vector3Int roundedLoc = Vector3Int.RoundToInt(node.Anchor);
+            AllNodes.TryGetValue(roundedLoc, out List<INavNode> dictNodes);
+            List<INavNode> newNodes = dictNodes ?? new List<INavNode>();
+            newNodes.Add(node);
+            AllNodes.Add(Vector3Int.RoundToInt(node.Anchor), newNodes);
         }
 
         public void NodeRemoved(INavNode node) {
             node.NavNodeLinks.ToList().ForEach( otherLink => {
                 otherLink.LinkedNavNode.NavNodeLinks.ToList().RemoveAll( link => link.LinkedNavNode.Equals(node));
             });
-            AllNodes.Remove(node.Anchor);
+            AllNodes.Remove(Vector3Int.RoundToInt(node.Anchor));
         }
 
-        public void NodesRemoved(Vector2 position) {
+        public void NodesRemoved(Vector2Int position) {
             foreach (INavNode node in GetNodes(position))
             {
                 NodeRemoved(node);
@@ -66,11 +70,11 @@ namespace AStarSquares
 
         public List<INavNode> GetNodesInRadius(Vector3 position, int radius) {
             List<INavNode> nodes = new List<INavNode>();
-
+            Vector3Int roundedPosition = Vector3Int.RoundToInt(position);
             for (int y = -radius; y <= radius; y++) {
                 for (int x = -radius; x <= radius; x++) {
                     if ((x * x) + (y * y) <= radius * radius) {
-                        nodes.AddRange(GetNodes(new Vector2(x + position.x,y + position.z)));
+                        nodes.AddRange(GetNodes(new Vector2Int(x + roundedPosition.x,y + roundedPosition.z)));
                     }
                 }
             }
@@ -79,22 +83,23 @@ namespace AStarSquares
 
         public IEnumerable<INavNode> GetNeighborNodes(INavNode node) {
             List<INavNode> nodes = new List<INavNode>();
-
+            Vector3Int roundedAnchor = Vector3Int.RoundToInt(node.Anchor);
             for (int y = -1; y <= 1; y++) {
                 for (int x = -1; x <= 1; x++) {
-                    nodes.AddRange(GetNodes(new Vector2(x + node.Anchor.x,y + node.Anchor.z)));
+                    nodes.AddRange(GetNodes(new Vector2Int(x + roundedAnchor.x,y + roundedAnchor.z)));
                 }
             }
             return nodes;
         }
 
 
-        public IEnumerable<INavNode> GetNodes(Vector2 position) {
-            return AllNodes.Where(node => node.Key.x == position.x).Where(node => node.Key.z == position.y).Select(kvp => kvp.Value);
+        public IEnumerable<INavNode> GetNodes(Vector2Int position) {
+            Debug.Log(AllNodes.Where(entry => entry.Key.x == position.x).Where(entry => entry.Key.z == position.y).SelectMany(entry => entry.Value).Count());
+            return AllNodes.Where(node => node.Key.x == position.x).Where(node => node.Key.z == position.y).SelectMany(kvp => kvp.Value);
         }
 
-        public IEnumerable<INavNode> GetNodes(Vector3 position) {
-            return GetNodes(new Vector2(position.x, position.z));
+        public IEnumerable<INavNode> GetNodes(Vector3Int position) {
+            return GetNodes(new Vector2Int(position.x, position.z));
         }
 
         private float GetDistance(Vector3 from, Vector3 to)
