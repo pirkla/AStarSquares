20e2ea23d7e955c5e1f028395b506005282bdf73
andrew.pirkl <andrew.pirkl@gmail.com>
2022-01-02 02:57:38 -0600

    arrays arent blittable

--- NavCostNode.cs
+++ NavCostNode.cs
@@ -3,19 +3,22 @@ using System;
 using Unity.Mathematics;
 using UnityEngine;
 
+
 namespace AStarSquares {
-    public struct NavCostNode: IComparable
+    public struct NavCostNode
     {
-        public int CompareTo(object other) {
-            if (other == null) return 1;
-            return this.FCost.CompareTo(((NavCostNode)other).FCost);
-        }
-        public Vector3Int? FromIndex;
-        public INavNode NavNode;
-        public NavNodeLink FromLink;
+        public int3 FromIndex;
+        public int Distance;
         public int GCost;
         public int HCost;
-        public int FCost => GCost + HCost;
-        public Vector3Int Index;
+        public int FCost;
+        public int3 Index;
+
+        public NavCostNodeLink[] CostNodeLinks;
+    }
+
+    public struct NavCostNodeLink {
+        public int Distance;
+        public int3 LinkedIndex;
     }
 }
